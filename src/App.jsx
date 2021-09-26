d2c92cb2bb2d26503111ee9672a93c99b02c9807
F34r Th3 R34per <danielpisani12@hotmail.com>
2021-09-26 15:20:35 -0400

    ability to 'add to cart' and to 'proceed to checkout'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect}  from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -6,16 +6,26 @@ import Products from './Products'
 import { Routes, Route } from "react-router-dom"
 import Detail from "./Detail";
 import Cart from "./Cart"
+import Checkout from "./Checkout"
 
 export default function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? []
+    } catch {
+      console.error("The cart could not be parsed into JSON.")
+      return []
+    }
+  })
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart])
 
   function addToCart(id, sku) {
     setCart( (items) => {
       const itemInCart = items.find((i) => i.sku === sku);
       if (itemInCart) {
         // Return new array with the matching item replaced
-        return items.imap( (i) => i.sku === sku ? {...i, quantity: i.quantity + 1} : i)
+        return items.map( (i) => i.sku === sku ? {...i, quantity: i.quantity + 1} : i)
       } else {
         // Return new array with the new item appended
         return [...items, { id, sku, quantity: 1}]
@@ -25,8 +35,14 @@ export default function App() {
 
   function updateQuantity(sku, quantity) {
     setCart( (items) => {
-      return items.map( (i) => i.sku === sku ?  {...i, quantity} : i)
+      return quantity === 0 
+      ? items.filter( (i) => i.sku !== sku)
+      : items.map( (i) => i.sku === sku ?  {...i, quantity} : i)
     })
+  }
+
+  function emptyCart() {
+    setCart([])
   }
 
   return (
@@ -39,6 +55,7 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>} />
             <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>} />
+            <Route path="/checkout" element={<Checkout cart={cart} emptyCart={emptyCart} />} />
           </Routes>
         </main>
       </div>
